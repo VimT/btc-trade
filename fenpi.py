04946a7feb50d0bbbb091612775862812876908e
taotaoli <taotaoli@tencent.com>
2017-08-02 15:45:39 +0800

    [fix] pythonic

--- fenpi.py
+++ fenpi.py
@@ -10,73 +10,77 @@ rate = 1.01
 sell_price_abs = 30
 
 
+class Part:
+    def __init__(self, buy_flag, buy_price, low_macd, high_macd, high_sel_price):
+        self.buy_flag = buy_flag
+        self.buy_price = buy_price
+        self.low_macd = low_macd
+        self.high_macd = high_macd
+        self.high_sel_price = high_sel_price
+
+
 def main():
     Log(exchange.GetAccount())
     Log("this is test begin")
-    buy_flag = {}
-    buy_price = {}
-    low_macd = {}
-    high_macd = {}
-    high_sel_price = {}
-    for i in range(0, N, 1):
-        buy_flag[i] = 0
-        buy_price[i] = 0
-        low_macd[i] = 99999
-        high_macd[i] = 0
-        high_sel_price[i] = 0
+
+    parts = []
+    for i in range(N):
+        parts[i] = Part(0, 0, 99999, 0, 0)
 
     has_buy = 0
-    can_buy = 1
+    can_buy = True
     lowest = 99999
 
     while True:
         recs = exchange.GetRecords(PERIOD_M5)
-        macd = TA.MACD(recs, 12, 26, 9)
+        macd, _, _ = TA.MACD(recs, 12, 26, 9)
         acc = exchange.GetAccount()
         balance = acc.Balance
         stocks = acc.Stocks
         ticker = exchange.GetTicker()
-        Log(acc)
-        Log("行情:" + str(ticker.Buy) + " MACD:" + str(macd[0][len(macd[0]) - 1]) + "  " + str(
-            macd[0][len(macd[0]) - 2]))
-        if lowest - 100 > ticker.Buy or macd[0][len(macd[0]) - 1] > 0:
-            can_buy = 1
 
-        for i in range(0, N, 1):
-            if buy_flag[i] == 0:
-                if macd[0][len(macd[0]) - 1] < buy_abs and macd[0][len(macd[0]) - 1] < low_macd[i]:
-                    low_macd[i] = macd[0][len(macd[0]) - 1]
-                if macd[0][len(macd[0]) - 1] < buy_abs and macd[0][len(macd[0]) - 1] - low_macd[i] > buy_cl_abs and can_buy == 1:
+        last1 = macd[-1]
+        last2 = macd[-2]
+
+        Log(acc)
+        Log(f"行情:{ticker.Buy} MACD:{last1}  {last2}")
+
+        if lowest - 100 > ticker.Buy or last1 > 0:
+            can_buy = True
+
+        for i in parts:
+            if i.buy_flag == 0:
+                if last1 < buy_abs and last1 < i.low_macd:
+                    i.low_macd = last1
+                if last1 < buy_abs and last1 - i.low_macd > buy_cl_abs and can_buy:
                     buy = balance / (N - has_buy)
                     exchange.Buy(buy)
                     has_buy += 1
                     balance -= buy
-                    buy_flag[i] = 1
-                    can_buy = 0
-                    buy_price[i] = ticker.Buy
+                    i.buy_flag = 1
+                    can_buy = False
+                    i.buy_price = ticker.Buy
                     lowest = ticker.Buy
-                    high_macd[i] = 0
-                    high_sel_price[i] = 0
-                    Log("买入价格:" + str(ticker.Buy) + " MACD:" + str(macd[0][len(macd[0]) - 1]) + "  " + str(
-                        macd[0][len(macd[0]) - 2]))
-            if buy_flag[i] == 1:
-                if macd[0][len(macd[0]) - 1] > sell_abs and macd[0][len(macd[0]) - 1] > high_macd[i]:
-                    high_macd[i] = macd[0][len(macd[0]) - 1]
+                    i.high_macd = 0
+                    i.high_sel_price = 0
+                    Log(f"买入价格:{ticker.Buy} MACD:{last1}  {last2}")
+            if i.buy_flag == 1:
+                if last1 > sell_abs and last1 > i.high_macd:
+                    i.high_macd = last1
 
-                if macd[0][len(macd[0]) - 1] > sell_abs and ticker.Sell > high_sel_price[i]:
-                    high_sel_price[i] = ticker.Sell
+                if last1 > sell_abs and ticker.Sell > i.high_sel_price:
+                    i.high_sel_price = ticker.Sell
 
-                if macd[0][len(macd[0]) - 1] > sell_abs and high_macd[i] - macd[0][
-                            len(macd[0]) - 1] > sel_cl_abs and ticker.Sell / buy_price[i] >= rate and high_sel_price[
-                    i] - ticker.Sell > sell_price_abs:
+                if last1 > sell_abs and i.high_macd - last1 > sel_cl_abs \
+                        and ticker.Sell / i.buy_price >= rate \
+                        and i.high_sel_price - ticker.Sell > sell_price_abs:
                     sell = stocks / has_buy
                     exchange.Sell(sell)
                     has_buy -= 1
                     stocks -= sell
-                    buy_flag[i] = 0
-                    low_macd[i] = 99999
-                    Log("卖出价格:" + str(ticker.Sell) + " MACD:" + str(macd[0][len(macd[0]) - 1]) + "  " + str(
-                        macd[0][len(macd[0]) - 2]))
+                    i.buy_flag = 0
+                    i.low_macd = 99999
+                    Log(f"卖出价格:{ticker.Sell} MACD:{last1}  {last2}")
                     # Log(str(macd[2][len(macd[2])-1])+"  "+str(macd[2][len(macd[2])-2]))
         Sleep(60000)
 
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import argparse
-
+# coding=utf-8
 from trade import *
 
 buy_abs = -20
@@ -17,31 +16,36 @@ def main():
     high_price = 0
     while True:
         recs = exchange.GetRecords(PERIOD_M5)
-        macd = TA.MACD(recs, 12, 26, 9)
+        macd, _, _ = TA.MACD(recs, 12, 26, 9)
         acc = exchange.GetAccount()
         ticker = exchange.GetTicker()
-        Log("行情：" + str(ticker.Buy) + " MACD:" + str(macd[0][len(macd[0]) - 1]) + "  " + str(
-            macd[0][len(macd[0]) - 2]))
+
+        last1 = macd[-1]
+        last2 = macd[-2]
+
+        Log(f"行情:{ticker.Buy} MACD:{last1}  {last2}")
+
+        Log("行情：" + str(ticker.Buy) + " MACD:" + str(last1) + "  " + str(
+            last2))
         if buy_flag == 0:
-            if macd[0][len(macd[0]) - 1] < buy_abs and macd[0][len(macd[0]) - 1] < low_price:
-                low_price = macd[0][len(macd[0]) - 1]
-            if macd[0][len(macd[0]) - 1] < buy_abs and macd[0][len(macd[0]) - 1] - low_price > buy_cl_abs:
+            if last1 < buy_abs and last1 < low_price:
+                low_price = last1
+            if last1 < buy_abs and last1 - low_price > buy_cl_abs:
                 exchange.Buy(acc.Balance)
                 buy_flag = 1
                 buy_price = ticker.Buy
                 high_price = 0
-                Log("买入价格:" + str(ticker.Buy) + " MACD:" + str(macd[0][len(macd[0]) - 1]) + "  " + str(
-                    macd[0][len(macd[0]) - 2]))
+                Log(f"买入价格:{ticker.Buy} MACD:{last1}  {last2}")
+
         else:
-            if macd[0][len(macd[0]) - 1] > sell_abs and macd[0][len(macd[0]) - 1] > high_price:
-                high_price = macd[0][len(macd[0]) - 1]
-            if macd[0][len(macd[0]) - 1] > sell_abs and high_price - macd[0][
-                        len(macd[0]) - 1] > sel_cl_abs and ticker.Sell / buy_price >= 1.01:
+            if last1 > sell_abs and last1 > high_price:
+                high_price = last1
+            if last1 > sell_abs and high_price - last1 > sel_cl_abs and ticker.Sell / buy_price >= 1.01:
                 exchange.Sell(acc.Stocks)
                 buy_flag = 0
                 low_price = 99999
-                Log("卖出价格:" + str(ticker.Sell) + " MACD:" + str(macd[0][len(macd[0]) - 1]) + "  " + str(
-                    macd[0][len(macd[0]) - 2]))
+                Log(f"卖出价格:{ticker.Sell} MACD:{last1}  {last2}")
+
         # Log(str(macd[2][len(macd[2])-1])+"  "+str(macd[2][len(macd[2])-2]))
         Sleep(60000)
 
