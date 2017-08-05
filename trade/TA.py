dcb6643e21c14c3b39b66f636631e14bebf5c386
VimT <vimt@foxmail.com>
2017-08-06 00:44:41 +0800

    [add] BOLL for TA

--- trade/TA.py
+++ trade/TA.py
@@ -12,3 +12,8 @@ def MACD(records, short, long, period):
     a, b, c = talib.MACD(np.array([i.Close for i in records]), short, long, period)
     c = 2 * c
     return c.tolist(), a.tolist(), b.tolist()
+
+
+def BOLL(records, period, multiplier):
+    upper, mid, lower = talib.BBANDS(np.array([i.Close for i in records]), period, multiplier, multiplier)
+    return upper.tolist(), mid.tolist(), lower.tolist()
